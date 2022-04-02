eb92c4613d03a63aeb9a918dc10c691accf25bb5
RSandesh15 <fannyvalette45@gmail.com>
2022-04-02 10:32:28 -0400

    new1

--- cypress/integration/2-advanced-examples/firsttest.js
+++ cypress/integration/2-advanced-examples/firsttest.js
@@ -8,3 +8,45 @@ describe('Test 1', function (){
         
     })
 })
+
+describe('Test 2', function (){
+    it('Test 2', function(){
+        cy.visit("http://localhost:3000/SellerLogin")
+        cy.get('button').should('contain', 'Button')   
+    })
+})
+
+describe('Test 3', function (){
+    it('Test 3', function(){
+        cy.visit("http://localhost:3000/SellerUploadPage")
+        cy.get('button').should('contain', 'Button')   
+    })
+})
+
+describe('Test 4', function (){
+    it('Test 4', function(){
+        cy.visit("http://localhost:3000/SignUp")
+        cy.get('button').should('contain', 'Button')   
+    })
+})
+
+describe('Test 5', function (){
+    it('Test 5', function(){
+        cy.visit("http://localhost:3000/Checkout")
+        cy.get('button').should('contain', 'Button')   
+    })
+})
+
+describe('Test 6', function (){
+    it('Test 6', function(){
+        cy.visit("http://localhost:3000/Dashboard")
+        cy.get('button').should('contain', 'Button')   
+    })
+})
+
+describe('Test 7', function (){
+    it('Test 7', function(){
+        cy.visit("http://localhost:3000/SellerLogin")
+        cy.get('button').should('contain', 'Button')   
+    })
+})
